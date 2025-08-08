b33bef2f0ee971a26b67afd46381c2a2b87c1fe5
remilkies <160505182+remilkies@users.noreply.github.com>
2025-08-08 11:48:03 +0200

    added movies api

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
+const url = 'https://imdb236.p.rapidapi.com/api/imdb/search?type=movie&genre=Drama&rows=25&sortOrder=ASC&sortField=id';
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': 'f6f00d2ab9msh59e2e58dc19f7bbp1247c7jsn1ab6efafe253',
+		'x-rapidapi-host': 'imdb236.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
+
 console.log("Welcome to Spoilt Milk")
 
 const rem = "Remazani Malade Ngalwana";// this is a string
@@ -53,4 +70,5 @@ function search_sections() {
   
     list.style.display = anyVisible ? 'block' : 'none';
   }
+
 
